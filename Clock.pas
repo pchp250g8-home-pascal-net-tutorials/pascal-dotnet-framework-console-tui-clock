15739daf7f02401c8bb110fc319a7c8deda7d8e6
Alexander <pchp250g8@tutamail.com>
2025-12-17 19:50:55 +0300

    Clock

---
+++ Clock.pas
@@ -0,0 +1,13 @@
+﻿uses
+  System,
+  System.Threading;
+begin
+  Console.Clear();
+  Console.CursorVisible := False;
+  Repeat
+    var currentTime := DateTime.Now;
+    Console.SetCursorPosition(1, 1);
+    Console.Write('Time:' + currentTime.ToLongTimeString());
+    Thread.Sleep(1000);
+  Until Console.KeyAvailable;
+end.
